95b92cc0ddee7269a32383cdbb4c4afc39296c0f
BOMBELLA <nmasionyena@gmail.com>
2026-05-18 17:11:51 +0100

    phase gate day one

---
+++ DAYONE/PYTHON/guessing_app.py
@@ -0,0 +1,43 @@
+import random
+
+for random_number  in range(1,100):
+
+       value = int(input(" Enter any value: "))    
+  
+
+#       value = int(input(" Enter any value: "))      
+   
+       if value > random_number:
+              
+              print("The number is higher")
+
+       elif value < random_number:
+              print("The number is lower")
+
+       elif value < 100:
+              print("Does not count as an attempt")
+
+       elif value > 100:
+              print("This is an error")
+       
+       elif value = 5:
+              break;
+
+       
+
+
+
+
+#for digit in range(6,10):
+#
+#       value = int(input(" Enter any digit: "))      
+#       
+#       if digit < random_number:
+#
+#              print("The number is lower")
+#              
+
+
+              
+
+#       print(random.randrange(1,100)
---
+++ DAYONE/PYTHON/kata.py
@@ -0,0 +1,7 @@
+word = "Hello  World"
+
+for count in range(1,11):
+
+       print("  " ,word, "  ","  ")
+
+      
