4e53ec3e9be04a4221dec0efe80efce41bd1f49e
BOMBELLA <nmasionyena@gmail.com>
2026-05-19 17:24:37 +0100

    levelfour

---
+++ DAYTWO/JAVA/LEVEL FOUR/Tasks.java
@@ -0,0 +1,261 @@
+public class Tasks{
+
+public static int addTwoNumbers(int number1 , int number2){
+
+       int sum = number1 + number2;
+
+       return sum;
+}
+
+
+public static int getEvenNumbers(int number){
+
+if(number % 2 == 0){
+
+return evenNumber;
+}
+
+
+}
+public static int getSquareOfNumbers(int numberOne ,int numberTwo){
+
+int square = numberOne * numberTwo;
+
+return square;
+}
+
+
+public static int checkForPrimeNumbers(int number){
+
+int primeNumber =  12;
+
+return primeNumber
+}
+
+
+public static int getLargestOfThreeNumbers(int numbers){
+
+
+int expectedLargestNumber = 90;
+
+return largestNumber;
+
+}
+
+public static int calculateSimpleIntrest(int principal,int rate, int time  / 100){
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static void main(String [] args){
+       
+int number1 = 5;
+int number2 = 5;
+int sum = number1 + number2;
+System.out.println(sum);
+
+int  evenNumbers = ;
+
+System.out.println(evenNumbers);
+
+int numberOne = 2;
+int numberTwo = 2;
+int result = numberOne * numberTwo;
+System.out.println(result);
+
+int numbers = 45,18,90;
+
+int expectedLargestNumber = 90;
+
+System.out.println(expectedLargestNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
