4ebe612407a6256315539b01e8e3ecddefe25bbf
BOMBELLA <nmasionyena@gmail.com>
2026-05-20 12:26:45 +0100

    FacebookLikeSystem

---
+++ DAY THREE/JAVA/MORNING SESSION/FireDrill1.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class FireDrill1{
+
+       public static String testNoOneLikedThePosts(String[] names){
+
+       String names = names [0];
+              
+
+//       String [] names = new String [numbers.length];
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ DAY THREE/JAVA/MORNING SESSION/FireDrillTest1.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FireDrillTest1{
+
+              @Test
+    public void testThatNoOneLikedAnycThePost(){
+      
+       
+       String [] names = new String[0];
+
+       String expectedNames = names [0];
+
+       String actualNames = FireDrill1.testThatNoOneLikedPosts(names);
+
+       assertEquals(actual, expected);
+
+
+
+}
+
+       
+         
+       
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
