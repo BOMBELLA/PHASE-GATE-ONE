2c6804085138dfd0f4d762d213e824416cf984d5
BOMBELLA <nmasionyena@gmail.com>
2026-05-20 16:08:53 +0100

    miniparkinglot

---
+++ DAY THREE/JAVA/AFTERNOON SESSION/MiniParkingLot.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+import java.util.Random;
+
+import java.util.Arrays;
+
+public class MiniParkingLot{
+
+       public static int getThatNoCarExists(int[] parkingSpaces){
+
+       int [] parkingSpaces = new int [0];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ DAY THREE/JAVA/AFTERNOON SESSION/MiniParkingLotTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MiniParkingLotTest{
+
+              @Test
+    public void  testThatNoCarExists_NoParkingSpace (){
+
+       int result = Mini.parkingLot(0,0);
+
+       assertEquals(0, result (0));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+      
+    
